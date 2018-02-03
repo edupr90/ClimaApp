f6ed1816566f6afb250e670826b42df937aeb19a
Eduardo Velez <evelezng@gmail.com>
2018-02-02 20:19:55 -0400

    Added APP ID

--- Clima-iOS11-master/Clima/WeatherViewController.swift
+++ Clima-iOS11-master/Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "b13baeedcb3557f83cba623afb506aa0"
     
 
     //TODO: Declare instance variables here
